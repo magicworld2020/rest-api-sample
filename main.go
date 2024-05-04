8134244f929d1e463d20ffb14fb5628844ec1752
wang <wang@JPC00220890.local>
2024-05-04 21:59:16 +0900

    20240504

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
 	"github.com/magicworld2020/rest-api-sample/controller"
 )
 
 func main() {
 	// Load TLS certificate and private key
-	certFile := "demo/certificate.crt"
-	keyFile := "demo/private.key"
+	// certFile := "demo/certificate.crt"
+	// keyFile := "demo/private.key"
 	router := gin.Default()
 	router.POST("/signup", controller.CreateUser)
 	router.GET("/users/:id", controller.GetUserByUserID)
 	router.PATCH("/users/:id", controller.UpdateUser)
 	router.POST("/close", controller.DeleteUser)
-
+	router.Run(":8080")
 	// Run HTTPS server
-	err := router.RunTLS(":443", certFile, keyFile)
-	if err != nil {
-		log.Fatalf("Failed to start HTTPS server: %v", err)
-	}
+	// err := router.RunTLS(":443", certFile, keyFile)
+	// if err != nil {
+	// 	log.Fatalf("Failed to start HTTPS server: %v", err)
+	// }
 }
